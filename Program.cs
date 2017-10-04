06161253af744e0d89231a55d73e3131c1b71d96
dvalverde <32370553+dvalverde@users.noreply.github.com>
2017-10-03 20:05:16 -0600

    Código sujerido para  natural join
    esta operación requiere 2 consultas, una para determinar los atributos iguales y otra para la consulta en si, con este código es posible generar un predicado adecuado, si funciona adecuadamente se puede usar para división

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public String comprobareq_msj(String[] atributos, String tabla1, String tabla2)
+        {
+            String mensj = "";
+            foreach (String atrib in atributos)
+            {
+                mensj += tabla1 + "." + atrib + "=" + tabla2 + "." + atrib + " and ";
+            }
+            mensj = mensj.Remove(mensj.Length - 5);
+            return mensj;
+        }
+        static void Main(string[] args)
+        {
+            Program a=new Program();
+            string[] atributos = { "atr1", "atr2", "atr3" };
+            Console.WriteLine(a.comprobareq_msj(atributos, "tabla1", "tabla2"));
+            Console.Read();
+        }
+    }
+}
