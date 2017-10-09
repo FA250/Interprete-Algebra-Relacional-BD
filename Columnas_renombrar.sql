5e65a3b6c5ee7b162b2ea3ef10c3a4b5798d0a1d
FA250 <faph.2009@hotmail.com>
2017-10-09 14:47:12 -0600

    Procedimientos almacenados 0.95

---
+++ Columnas_renombrar.sql
@@ -0,0 +1,18 @@
+USE [bdproy1]
+GO
+/****** Object:  StoredProcedure [dbo].[Columnas_agregacion]    Script Date: 10/9/2017 12:56:02 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+alter PROCEDURE Columnas_renombrar @BD varchar(max), @tabla varchar(max), @nuevaTabla varchar(max),@atributos varchar(max)
+AS
+BEGIN
+	execute('use '+ @BD+ '; select * into '+@nuevaTabla+' from (select '+@atributos+' from '+@tabla+') as origen')
+	
+END
